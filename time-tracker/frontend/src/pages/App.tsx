11b4d924cab9faa120246b6f06b158a9a079f0a3
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-08-20 09:23:02 +0200

    description

--- time-tracker/frontend/src/pages/App.tsx
+++ time-tracker/frontend/src/pages/App.tsx
@@ -96,7 +96,7 @@ function App() {
                 <header className="mb-12 flex items-center justify-between">
                     <div>
                         <h1 className="text-4xl font-bold text-gray-100">Time Tracker</h1>
-                        <p className="text-gray-400">Track your freelance work with ease.</p>
+                        <p className="text-gray-400">Track your work with ease.</p>
                     </div>
                     <button className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">
                         Manage Customers
