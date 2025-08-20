ffd1541288b4ab2f61d24adc3433e9f5914a3ba4
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-08-20 09:30:43 +0200

    manage customesr

--- time-tracker/frontend/src/pages/ManageCustomers.tsx
+++ time-tracker/frontend/src/pages/ManageCustomers.tsx
@@ -9,9 +9,18 @@ export default function () {
                     <p className="text-gray-400">Manage customers.</p>
                 </div>
                 <Link to="/" className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">
-                    Back
+                    Time Tracker
                 </Link>
             </header>
+
+            <main>
+                <div className="bg-gray-800 rounded-lg shadow-lg p-6 mb-8 flex items-center justify-between">
+                    <h2 className="text-2xl font-bold mb-4">New Customer</h2>
+                </div>
+                <div className="bg-gray-800 rounded-lg shadow-lg p-6 mb-8 flex items-center justify-between">
+                    <h2 className="text-2xl font-bold mb-4">Customers</h2>
+                </div>
+            </main>
         </>
     )
 }
