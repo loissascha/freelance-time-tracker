95f61113f5581d643f754a725f644ee51749fa0f
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-08-20 10:08:46 +0200

    auto run migrations

--- time-tracker/internal/db/main.go
+++ time-tracker/internal/db/main.go
@@ -2,10 +2,15 @@ package db
 
 import (
 	"database/sql"
+	"embed"
 	"fmt"
+	"github.com/pressly/goose/v3"
 
 	_ "github.com/mattn/go-sqlite3"
 )
+
+//go:embed migrations/*.sql
+var embedMigrations embed.FS
 
 type Db struct {
 	Db *sql.DB
@@ -13,9 +18,6 @@ type Db struct {
 
 func InitDb() *Db {
 	dbFileName := "tracker.db"
-
-	// _, err := os.Stat(dbFileName)
-	// needsSetup := os.IsNotExist(err)
 
 	db, err := sql.Open("sqlite3", dbFileName)
 	if err != nil {
@@ -31,48 +33,29 @@ func InitDb() *Db {
 
 	fmt.Println("Successfully connected to the database.")
 
-	// Schema migration
-	// if needsSetup {
-	// 	// createSchema(db)
-	// }
-
-	return &Db{
+	d := &Db{
 		Db: db,
 	}
+	err = d.runMigrations()
+	if err != nil {
+		panic(err)
+	}
+	return d
 }
 
-// func createSchema(db *sql.DB) error {
-// 	// The SQL statement to create the customers table.
-// 	// `INTEGER PRIMARY KEY AUTOINCREMENT` creates a unique ID for each new row.
-// 	// `TEXT NOT NULL` means the field must have a value.
-// 	// `UNIQUE` ensures we don't have duplicate customer names.
-// 	createCustomersTableSQL := `
-// 	CREATE TABLE customers (
-// 		"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-// 		"name" TEXT NOT NULL UNIQUE,
-// 		"created_at" DATETIME DEFAULT CURRENT_TIMESTAMP
-// 	);`
-//
-// 	// `Exec` is used for statements that don't return rows (like CREATE, INSERT, UPDATE, DELETE).
-// 	_, err := db.Exec(createCustomersTableSQL)
-// 	if err != nil {
-// 		return err
-// 	}
-//
-// 	// The SQL statement to create the time_entries table.
-// 	// `FOREIGN KEY(customer_id) REFERENCES customers(id)` creates a link between
-// 	// this table and the customers table. It ensures that a time entry can only
-// 	// be created for a customer that actually exists.
-// 	createTimeEntriesTableSQL := `
-// 	CREATE TABLE time_entries (
-// 		"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-// 		"customer_id" INTEGER NOT NULL,
-// 		"hours" REAL NOT NULL,
-// 		"description" TEXT,
-// 		"entry_date" DATETIME DEFAULT CURRENT_TIMESTAMP,
-// 		FOREIGN KEY(customer_id) REFERENCES customers(id)
-// 	);`
-//
-// 	_, err = db.Exec(createTimeEntriesTableSQL)
-// 	return err
-// }
+func (db *Db) runMigrations() error {
+	goose.SetBaseFS(embedMigrations)
+
+	err := goose.SetDialect("sqlite3")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Running database migrations...")
+	err = goose.Up(db.Db, "migrations")
+	if err != nil {
+		return err
+	}
+	fmt.Println("Migrations applied successfully.")
+	return nil
+}
